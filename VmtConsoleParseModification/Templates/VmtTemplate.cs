4884940b96e0b4354c1f7eef7a5551c37b9a1ddd
Shark-vil <shark_vil@mail.ru>
2020-08-25 19:20:29 +0300

    Creating a VMT file template

---
+++ VmtConsoleParseModification/Templates/VmtTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VmtConsoleParseModification.Templates
+{
+    public class VmtTemplate
+    {
+        public class VmtField
+        {
+            public string Key { get; set; }
+            public string Value { get; set; }
+
+            public VmtField(string Key, string Value)
+            {
+                this.Key = Key;
+                this.Value = Value;
+            }
+        }
+
+        public string MaterialType { get; set; }
+        public List<VmtField> Values { get; set; }
+
+        public VmtTemplate(string MaterialType, List<VmtField> Values)
+        {
+            this.MaterialType = MaterialType;
+            this.Values = Values;
+        }
+    }
+}
